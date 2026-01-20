f58d2c5ef00541a0c76a8eb5c6b9ea7206ee89d0
younow4 <manarharown4@gmail.com>
2026-01-20 18:09:31 +0200

    Update main.dart
    edit 222

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('Abdoooo'),
+            const Text('manaaar'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
