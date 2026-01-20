5030ac5c12959add752aebb5f1810c84d7f8d7b6
Linguini <linguiniteamapp@gmail.com>
2026-01-20 17:40:41 +0200

    Abdooo

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('hello it is me :'),
+            const Text('Abdoooo'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
