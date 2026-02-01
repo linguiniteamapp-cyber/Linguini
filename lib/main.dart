60f93910d2689c8731e659a6d8993f97dafef406
Linguini <linguiniteamapp@gmail.com>
2026-02-01 15:40:03 +0200

    chat

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:linguini_app/choose_account.dart';
 import 'package:linguini_app/login.dart';
 import 'package:linguini_app/profile_setup_1.dart';
 import 'splash_page.dart';
+import 'package:linguini_app/chat.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      home: ChatListPage(),
     );
   }
 }
